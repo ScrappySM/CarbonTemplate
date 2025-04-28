5e552e26cc0fb0ff4487f2b3df83614552678bfe
Ben McAvoy <ben.mcavoy@tutanota.com>
2025-04-28 23:45:48 +0100

    chore: cleanup

--- CarbonTemplate/src/dllmain.cpp
+++ CarbonTemplate/src/dllmain.cpp
@@ -1,24 +1,15 @@
-#include <Windows.h>
-#include <iostream>
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 
-#include "log.h"
+#include <Windows.h>
 
 // DllMain function which gets called when the DLL is loaded
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
-	// RAII object to handle console creation/destruction
-	// Simply having this line here will allow you to print to the games console
-	// Don't worry about it, but if you're interested feel free to look in `utils.h`
-	// for the implementation
-	Log_t logRAII;
-
 	if (dwReason == DLL_PROCESS_ATTACH) {
 		DisableThreadLibraryCalls(hModule);
-		std::cout << "DLL_PROCESS_ATTACH" << std::endl;
-		// Setup, hook, etc here
 	}
 
 	if (dwReason == DLL_PROCESS_DETACH) {
-		std::cout << "DLL_PROCESS_DETACH" << std::endl;
 		// Cleanup, unhooking, etc here
 	}
 
